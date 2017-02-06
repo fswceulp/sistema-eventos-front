1475d5e4a3027a489d8fdef79543eb1c45f5170c
Jackson Gomes <jackson.souza@gmail.com>
2017-02-06 18:13:13 -0300

    Utilização de eventos e propriedades, diretivas ngFor e ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import '../../public/css/style.css';
 })
 export class AppComponent {
   eventos: any[];
+  eventoSelecionado: any = null;
 
   constructor() {
     this.eventos = [
@@ -14,23 +15,39 @@ export class AppComponent {
         "id": 1,
         "nome": "XIX Congresso de Computação e Sistemas de Informação",
         "sigla": "ENCOINFO",
-        "ano": 2017,
-        "mes": "maio"
+        "inicio": "2017-05-09",
+        "termino": "2017-05-13",
+        "url": "http://ulbra-to.br/encoinfo",
+        "cidade": "Palmas",
+        "estado": "TO",
+        "local": "Centro Universitário Luterano de Palmas - CEULP/ULBRA"
       },
       {
         "id": 2,
         "nome": "XIII Simpósio Brasileiro de Sistemas de Informação",
         "sigla": "SBSI",
-        "ano": 2017,
-        "mes": "junho"
+        "inicio": "2017-06-05",
+        "termino": "2017-06-08",
+        "url": "http://www.sbc.org.br/sbsi2017",
+        "cidade": "Lavras",
+        "estado": "MG",
+        "local": "Universidade Federal de Lavras - UFLA"
       },
       {
         "id": 3,
         "nome": "XXXVII Congresso da Sociedade Brasileira de Computação",
         "sigla": "CSBC",
-        "ano": 2017,
-        "mes": "julho"
+        "inicio": "2017-07-02",
+        "termino": "2017-07-06",
+        "url": "http://www.sbc.org.br/csbc2017",
+        "cidade": "São Paulo",
+        "estado": "SP",
+        "local": "Universidade Presbiteriana Mackenzie"
       }
     ];
+  }
+
+  mostrarDetalhes(evento: any) : void {
+    this.eventoSelecionado = evento;
   }
 }
