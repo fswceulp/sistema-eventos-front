44aa3fb9d43950a0297e24d286685f522d12339d
GuilhermeJRA <guilhermedk1991@gmail.com>
2017-02-20 22:08:52 -0300

    component Rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {RodapeComponent} from "./rodape.component";
 @NgModule({
     imports: [
         BrowserModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        RodapeComponent
     ],
     bootstrap: [ AppComponent ]
 })
---
+++ src/app/rodape.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../public/css/style.css';
+@Component({
+    selector: 'rodape',
+    templateUrl: './rodape.component.html',
+})
+export class RodapeComponent { }
+
