9b9dc8df3bcbc5b7e9d0e9ff09766614fc072339
Jackson Gomes <jackson.souza@gmail.com>
2017-03-06 18:28:59 -0300

    Uso de formulários para cadastro de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,42 @@
 import { Component } from '@angular/core';
+import { Evento } from './Evento';
+
 import '../../public/css/style.css';
+
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  eventos: any[];
-  eventoSelecionado: any = null;
+  eventos: Evento[];
+  eventoSelecionado: Evento = null;
+  evento: Evento = new Evento(0, '', '');
+  enviado: boolean = false;
 
   constructor() {
     this.eventos = [
-      {
-        "id": 1,
-        "nome": "XIX Congresso de Computação e Sistemas de Informação",
-        "sigla": "ENCOINFO",
-        "inicio": "2017-05-09",
-        "termino": "2017-05-13",
-        "url": "http://ulbra-to.br/encoinfo",
-        "cidade": "Palmas",
-        "estado": "TO",
-        "local": "Centro Universitário Luterano de Palmas - CEULP/ULBRA"
-      },
-      {
-        "id": 2,
-        "nome": "XIII Simpósio Brasileiro de Sistemas de Informação",
-        "sigla": "SBSI",
-        "inicio": "2017-06-05",
-        "termino": "2017-06-08",
-        "url": "http://www.sbc.org.br/sbsi2017",
-        "cidade": "Lavras",
-        "estado": "MG",
-        "local": "Universidade Federal de Lavras - UFLA"
-      },
-      {
-        "id": 3,
-        "nome": "XXXVII Congresso da Sociedade Brasileira de Computação",
-        "sigla": "CSBC",
-        "inicio": "2017-07-02",
-        "termino": "2017-07-06",
-        "url": "http://www.sbc.org.br/csbc2017",
-        "cidade": "São Paulo",
-        "estado": "SP",
-        "local": "Universidade Presbiteriana Mackenzie"
-      }
+      new Evento(1, 'XIX Congresso de Computação e Sistemas de Informação', 'ENCOINFO'),
+      new Evento(2, 'XIII Simpósio Brasileiro de Sistemas de Informação', 'SBSI'),
+      new Evento(3, 'XXXVII Congresso da Sociedade Brasileira de Computação', 'CSBC')
     ];
   }
 
-  mostrarDetalhes(evento: any) : void {
+  preencherNovoEvento(): void {
+    this.evento = new Evento(0, "", "");
+  }
+
+  mostrarDetalhes(evento: Evento) : void {
     this.eventoSelecionado = evento;
+  }
+
+  onSubmit() : void {
+    console.log(this.evento);
+    this.eventos.push(this.evento);
+    this.enviado = true;
+  }
+
+  novoEvento() : void {
+    this.preencherNovoEvento();
   }
 }
