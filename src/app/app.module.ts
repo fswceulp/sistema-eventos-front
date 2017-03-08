cf70494587caa7d226ff287445e479b588350490
Jackson Gomes <jackson.souza@gmail.com>
2017-03-07 21:47:24 -0300

    Utilização do Bootstrap e do ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EventoManagerComponent } from './evento-manager.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        NgbModule.forRoot()
     ],
     declarations: [
         AppComponent,
