f81ef2cb61617beab0e487f693274208129895c9
ianmaiscedo@gmail.com <ianmaiscedo@gmail.com>
2017-03-12 22:36:20 -0300

    Formulário completo, tabela, excluir e editar

--- src/app/evento-manager.component.ts
+++ src/app/evento-manager.component.ts
@@ -8,32 +8,52 @@ import { Evento } from './Evento';
 export class EventoManagerComponent {
   eventos: Evento[];
   eventoSelecionado: Evento = null;
-  evento: Evento = new Evento(0, '', '');
+  eventoEditado:Evento = null;
+  editado:boolean = false;
+  evento: Evento = new Evento(0, "", "", '', '', '', '', '', '');
   enviado: boolean = false;
+  excluir: boolean = false;
+  texto: string = 'this is sample text';
 
   constructor() {
     this.eventos = [
-      new Evento(1, 'XIX Congresso de Computação e Sistemas de Informação', 'ENCOINFO'),
-      new Evento(2, 'XIII Simpósio Brasileiro de Sistemas de Informação', 'SBSI'),
-      new Evento(3, 'XXXVII Congresso da Sociedade Brasileira de Computação', 'CSBC')
+      new Evento(1, 'XIX Congresso de Computação e Sistemas de Informação', 'ENCOINFO', '2017-03-04', '2017-03-06', 'http://ulbra-to.br/encoinfo/site/', 'Palmas', 'TO', 'CEULP'),
+      new Evento(2, 'XIII Simpósio Brasileiro de Sistemas de Informação', 'SBSI', '2017-04-20', '2017-04-25', 'http://sbsi2016.ufsc.br/', 'Florianópolis', 'SC', 'Castelmar'),
+      new Evento(3, 'XXXVII Congresso da Sociedade Brasileira de Computação', 'CSBC', '2017-05-15', '2017-05-20', 'http://csbc2017.mackenzie.br/', 'São Paulo', 'SP', 'Mackenzie')
     ];
   }
 
   preencherNovoEvento(): void {
-    this.evento = new Evento(0, "", "");
+    this.evento = new Evento(0, "", "", '', '', '', '', '', '');
   }
-
-  mostrarDetalhes(evento: Evento) : void {
-    this.eventoSelecionado = evento;
+  
+  editarEvento(evento: Evento) : void{
+    this.evento = evento;
+	this.editado = true;
+    this.eventoEditado = evento;
+  }
+  
+  excluirEvento(evento: Evento) : void {
+	this.excluir = confirm("Deseja excluir o evento?");
+	if(this.excluir){
+		this.eventos.splice(this.eventos.indexOf(evento), 1);			
+	}
   }
 
   onSubmit() : void {
     console.log(this.evento);
-    this.eventos.push(this.evento);
+	if(this.editado){
+		this.eventos.splice(this.eventos.indexOf(this.eventoEditado),1,this.eventoEditado);
+		this.editado = false;
+    }
+    else{
+		this.eventos.push(this.evento);
+	}
     this.enviado = true;
   }
 
   novoEvento() : void {
+	this.editado = false;
     this.preencherNovoEvento();
   }
 }
