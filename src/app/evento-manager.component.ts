88faa3f8874a39991f7704cee1ee4351b7f07aa8
Jackson Gomes <jackson.souza@gmail.com>
2017-03-20 14:20:01 -0300

    select em cascata e validações

--- src/app/evento-manager.component.ts
+++ src/app/evento-manager.component.ts
@@ -10,6 +10,8 @@ export class EventoManagerComponent {
   eventoSelecionado: Evento = null;
   evento: Evento = new Evento(0, '', '');
   enviado: boolean = false;
+  estados: any[];
+  cidades: any[];
 
   constructor() {
     this.eventos = [
@@ -17,6 +19,34 @@ export class EventoManagerComponent {
       new Evento(2, 'XIII Simpósio Brasileiro de Sistemas de Informação', 'SBSI'),
       new Evento(3, 'XXXVII Congresso da Sociedade Brasileira de Computação', 'CSBC')
     ];
+    this.estados = [
+      {uf: 'TO', nome: 'Tocantins'},
+      {uf: 'GO', nome: 'Goiás'},
+      {uf: 'MG', nome: 'Minas Gerais'},
+      {uf: 'SP', nome: 'São Paulo'},
+      {uf: 'RJ', nome: 'Rio de Janeiro'}
+    ];
+    this.cidades = [
+      {nome: 'Palmas', uf: 'TO'},
+      {nome: 'Paraíso do Tocantins', uf: 'TO'},
+      {nome: 'Gurupi', uf: 'TO'},
+      {nome: 'Araguaína', uf: 'TO'},
+      {nome: 'Porto Nacional', uf: 'TO'},
+      {nome: 'Belo Horizonte', uf: 'MG'},
+      {nome: 'Goiânia', uf: 'GO'},
+      {nome: 'São Paulo', uf: 'SP'},
+      {nome: 'Rio de Janeiro', uf: 'RJ'}
+    ];
+  }
+
+  getCidades(uf: string) {
+    let lista: any[] = [];
+    for(let i = 0; i < this.cidades.length; i++) {
+      if (this.cidades[i].uf == uf) {
+        lista.push(this.cidades[i]);
+      }
+    }
+    return lista;
   }
 
   preencherNovoEvento(): void {
