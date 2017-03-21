c90733470aad58b7998bd23a271905de85a76963
Marcos <marcosnobre26@gmail.com>
2017-03-20 23:49:38 -0300

    Trabalho 2 Marcos Nobre

--- src/app/evento-manager.component.ts
+++ src/app/evento-manager.component.ts
@@ -10,12 +10,19 @@ export class EventoManagerComponent {
   eventoSelecionado: Evento = null;
   evento: Evento = new Evento(0, '', '');
   enviado: boolean = false;
+  ocultarCadastrar: boolean = true;
+  ocultarEvento: boolean = true;
+  verTodos: boolean = true;
+  eventoInd: boolean = false;
+  eventoIndividual: boolean = true;
+  confirmarr: Evento = null;
 
   constructor() {
     this.eventos = [
       new Evento(1, 'XIX Congresso de Computação e Sistemas de Informação', 'ENCOINFO'),
       new Evento(2, 'XIII Simpósio Brasileiro de Sistemas de Informação', 'SBSI'),
-      new Evento(3, 'XXXVII Congresso da Sociedade Brasileira de Computação', 'CSBC')
+      new Evento(3, 'XXXVII Congresso da Sociedade Brasileira de Computação', 'CSBC'),
+      new Evento(4, 'XXII Congresso da Associação de Programadores', 'CAP')
     ];
   }
 
@@ -24,7 +31,13 @@ export class EventoManagerComponent {
   }
 
   mostrarDetalhes(evento: Evento) : void {
+    this.eventoIndividual = !this.eventoIndividual;
     this.eventoSelecionado = evento;
+    this.verTodos = true;
+  }
+
+  mainController(){
+    $scope.goCats = false;
   }
 
   onSubmit() : void {
@@ -33,8 +46,44 @@ export class EventoManagerComponent {
     this.enviado = true;
   }
 
+  onClick() : void {
+    alert('Botão Clicado!');
+  }
+
+  ocultarCadastro() : void {
+    //this.ocultarCadastrar = !this.ocultarCadastrar;
+    this.ocultarCadastrar = false;
+    this.ocultarEvento = true;
+    this.verTodos = true;
+  }
+
+  ocultarEventos() : void {
+    //this.ocultarEvento = !this.ocultarEvento;
+    this.ocultarEvento = !this.ocultarEvento;
+    this.ocultarCadastrar = true;
+    this.verTodos = true;
+    this.eventoIndividual = true;
+  }
+
+  ocultarVerTodos() : void {
+    this.verTodos = !this.verTodos;
+    this.ocultarEvento = true;
+    //this.ocultarCadastrar = false;
+  }
+
   novoEvento() : void {
     this.preencherNovoEvento();
+  }
+
+  eventoIndividual(evento: Evento) : void {
+    this.eventoIndd=evento;
+    this.eventoInd=true;
+    this.ocultarEvento =false;
+  }
+
+  confirmar(evento: Evento) : void {
+    this.confirmarr=evento;
+    this.eventoIndividual = true;
   }
 
   /*-------------------------------------------------------------------------------------------------*/
@@ -50,5 +99,13 @@ export class EventoManagerComponent {
       }
     })
     this.eventos.splice(variavel, 1);
+    this.eventoIndividual = true;
+    this.ocultarEvento = !this.ocultarEvento;
+    this.confirmarr = Evento = null;
+  }
+
+  true(): void {
+    
+    return true;
   }
 }
