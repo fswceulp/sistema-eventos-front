5fb5a7df621d75607fc7e52f12fb975d6980698d
Jackson Gomes <jackson.souza@gmail.com>
2017-03-22 19:29:38 -0300

    Revert "agora vai"
    This reverts commit db432bf133b1317d4089f490cb6320f747a00faa.

--- src/app/Evento.ts
+++ src/app/Evento.ts
@@ -8,22 +8,18 @@ export class Evento {
     public cidade : string;
     public estado : string;
     public local : string;
+    public imagem : string;
 
-    constructor(id: number, nome: string, sigla: string, inicio?: string, termino?: string, url?: string, cidade?: string, estado?: string, local?: string) {
+    constructor(id: number, nome: string, sigla: string, inicio: string, termino: string, url: string, cidade: string, estado: string, local: string, imagem: string) {
         this.id = id;
         this.nome = nome;
         this.sigla = sigla;
-        if(inicio)
-          this.inicio=inicio;
-        if(termino)
-          this.termino=termino;
-        if(url)
-          this.url=url;
-        if(cidade)
-          this.cidade=cidade;
-        if(estado)
-          this.estado=estado;
-        if(local)
-          this.local=local;
+        this.inicio = inicio;
+        this.termino = termino;
+        this.url = url;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.local = local;
+        this.imagem = imagem;
     }
 }
